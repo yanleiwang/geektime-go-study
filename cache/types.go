ccf24e5ef7baf1a92d46300334dc573657f9a20b
wangyanlei <q123917515@gmail.com>
2023-04-20 10:43:31 +0800

    cache: 定义cache接口

---
+++ cache/types.go
@@ -0,0 +1,13 @@
+package cache
+
+import (
+	"context"
+	"time"
+)
+
+type Cache interface {
+	Get(ctx context.Context, key string) (any, error)
+	Set(ctx context.Context, key string, val any, expiration time.Duration) error
+	Delete(ctx context.Context, key string) error
+	Close() error
+}
